f80c9ba9b04e5f8731a6d64e04d19120bcd70c9f
Michael Ng <lunalescia@gmail.com>
2023-07-06 15:06:17 -0700

    test

--- index.js
+++ index.js
@@ -145,7 +145,11 @@ function isDoubleDigit(x, y) {
     robot.getPixelColor(x, y) === '000000' &&
     robot.getPixelColor(x, y+1) === '000000'&&
     robot.getPixelColor(x, y+2) === '000000'&&
+    robot.getPixelColor(x, y+3) === '000000'&&
+    robot.getPixelColor(x, y+4) === '000000'&&
+    robot.getPixelColor(x, y+5) === '000000'&&
     robot.getPixelColor(x, y+6) === '000000'&&
+    robot.getPixelColor(x, y+7) === '000000'&&
     robot.getPixelColor(x, y+8) === '000000'
   ) {
     return true;
