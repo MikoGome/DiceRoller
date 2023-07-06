cccd8200179ce360c9be57a8d8d10ac054625d69
Michael Ng <lunalescia@gmail.com>
2023-07-06 15:01:37 -0700

    test

--- index.js
+++ index.js
@@ -7,18 +7,16 @@ const readline = require('readline').createInterface({
 
 function main() {
   const desiredStats = {
-    STR: 4,
-    DEX: 4,
-    LUK: 5
+    INT: 10
   }
   
   setTimeout(() => {
-    rollDice(desiredStats);
-  }, 3000);
+    rollDice(desiredStats, 'min');
+  }, 5000);
 
 }
 
-function rollDice(desiredStats) {
+function rollDice(desiredStats, mode) {
   if(checkStats(desiredStats)) {
     robot.moveMouse(833, 484);
     robot.mouseClick();
@@ -26,7 +24,11 @@ function rollDice(desiredStats) {
     process.exit();
     return;
   }
-  if(robot.getPixelColor(1304, 662) === 'ee1111') {
+  if(
+    robot.getPixelColor(1304, 662) === 'ee1111' && 
+    robot.getPixelColor(672, 172) === 'ffdd55' && 
+    robot.getPixelColor(676, 131) === '555588'
+  ) {
     robot.moveMouse(916,400);
     robot.mouseClick();
     robot.moveMouseSmooth(getRandomCoords(910,920), getRandomCoords(395,405));
@@ -43,41 +45,74 @@ function checkStats(desiredStats) {
     INT: Infinity,
     LUK: Infinity
   }
-  //STR
-  if(is4(849, 339)) {
-    output.STR = 4;
-  } else if(is5(849, 339)){
-    output.STR = 5;
-  }
 
-  //DEX
-  if(is4(849, 359)) {
-    output.DEX = 4;
-  } else if(is5(849, 359)){
-    output.DEX = 5;
-  }
-
-  //INT
-  if(is4(849, 379)) {
-    output.INT = 4;
-  } else if(is5(849, 379)){
-    output.INT = 5;
-  }
-
-  //LUK
-  if(is4(849, 399)) {
-    output.LUK = 4;
-  } else if(is5(849, 399)){
-    output.LUK = 5;
+  if(mode === 'max') {
+    checkMax(output);
+  } else if(mode === 'min') {
+    checkMin(output);
   }
 
   //check
   for(const val in desiredStats) {
-    if(output[val] > desiredStats[val]) {
+    if(mode === 'max' && output[val] > desiredStats[val]) {
+      return false;
+    } else if(mode === 'min' && output[val] < desiredStats[val]) {
       return false;
     }
   }
   return true;
+}
+
+function checkMax(stats) {
+  //STR
+  if(is4(849, 339)) {
+    stats.STR = 4;
+  } else if(is5(849, 339)){
+    stats.STR = 5;
+  }
+
+  //DEX
+  if(is4(849, 359)) {
+    stats.DEX = 4;
+  } else if(is5(849, 359)){
+    stats.DEX = 5;
+  }
+
+  //INT
+  if(is4(849, 379)) {
+    stats.INT = 4;
+  } else if(is5(849, 379)){
+    stats.INT = 5;
+  }
+
+  //LUK
+  if(is4(849, 399)) {
+    stats.LUK = 4;
+  } else if(is5(849, 399)){
+    stats.LUK = 5;
+  }
+}
+
+function checkMin(stats) {
+  //STR
+  if(isDoubleDigit(848, 339)) {
+    stats.STR = 10;
+  }
+
+  //DEX
+  if(isDoubleDigit(848, 359)) {
+    stats.DEX = 10;
+  }
+
+  //INT
+  if(isDoubleDigit(848, 379)) {
+    stats.INT = 10;
+  }
+
+  //LUK
+  if(isDoubleDigit(848, 399)) {
+    stats.LUK = 10;
+  }
 }
 
 function is4(x, y) {
@@ -100,6 +135,18 @@ function is5(x, y) {
     robot.getPixelColor(x+1, y+5) === '000000' &&
     robot.getPixelColor(x+1, y+6) === '000000' &&
     robot.getPixelColor(x-2, y+8) === '000000'
+  ) {
+    return true;
+  }
+}
+
+function isDoubleDigit(x, y) {
+  if(
+    robot.getPixelColor(x, y) === '000000' &&
+    robot.getPixelColor(x, y+1) === '000000'&&
+    robot.getPixelColor(x, y+2) === '000000'&&
+    robot.getPixelColor(x, y+6) === '000000'&&
+    robot.getPixelColor(x, y+8) === '000000'
   ) {
     return true;
   }
